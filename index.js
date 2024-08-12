9329c56411d4d998f24dd7e47a6096473845cace
King Odo <Ogblowoodo@gmail.com>
2024-08-12 12:38:40 +0000

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-
-function getComputerChoice (){
-    Math.random("rock", "Paper", "Scrissors");
+let choice=["rock", "paper", "scissors"];
+function getComputerChoice (compchoice){
+ 
+    return Math.random();
 }
+if(compchoice==0){
+    console.log("ROCK");
+}
+else 
+if(compchoice==1){
+    console.log("SCISSORS");
+} else if(compchoice>0 && compchoice<1){
+    console.log("PAPER");
+}
+
+
+function getHumanChoice(){
+    let choice= prompt("Enter your Choice: ");
+}
+
+
