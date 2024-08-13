d6e18e462e1ef55c1032b24494d8b84b27a74e4f
King Odo <Ogblowoodo@gmail.com>
2024-08-13 09:50:21 +0000

    playround function check

--- index.js
+++ index.js
@@ -8,32 +8,38 @@ function getComputerChoice(){
      compchoice=Math.random() * choice.length;
 
 if(compchoice<0.33){
-    return "ROCK";
+    return "Computer says: ROCK";
 }else 
 if(compchoice>=0.34 && compchoice<=0.66){
-    return  "PAPER";
+    return  "Computer says: PAPER";
 }else
  while (compchoice >=0.67 && compchoice<=1){
-    return "SCISSORS";
+    return "Computer says: SCISSORS";
  }
 }
 console.log(getComputerChoice()); //view computer choice.
 
 function getHumanChoice(){
-    hchoice = parseInt(prompt("Enter your choice: "));
-   if(hchoice==1){
+    hchoice = (prompt("Enter your choice: "));
+   if(hchoice==1 || hchoice=="rock"){
     return "Your choice is: ROCK";
    }else
-   if (hchoice==2){
+   if (hchoice==2|| hchoice=="paper"){
     return "Your choice is: PAPER";
    }else
-   if(hchoice==3){
+   if(hchoice==3|| hchoice=="scissors"){
     return "Your choice is: SCISSORS";
    }
-
+   return hchoice.charAt(0).toUpperCase()+hchoice.slice(1).toLowerCase;
 }
-console.log(getHumanChoice());//User choice is between 1-3, in the order of Rock-Paper-Scissors.
+console.log(getHumanChoice());//User choice is between 1-3, in the order Rock-Paper-Scissors.
 
 function playRound(humanchoice, computerchoice){
+    return humanchoice.charAt(0).toUpperCase();
+
 
 }
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
